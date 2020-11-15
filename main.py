c7d94a5077f2e223c4106019b3c65731ca9a5e55
Karuzzzo <clastone@mail.ru>
2020-11-15 10:07:51 +0300

    Networking now work

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 from web3 import Web3
 from eth_account import Account
-my_address = "0x79387B8a5d09E7B1613532a180bdc3d9Ef2aAB25"
-private_key = "9412462ca9e849426141fef9a7f92116588035324d7c1cdda9a51cd4989b1859"
-#rinkeby_link = "https://rinkeby.infura.io/v3/a9ea539cf3a348fead71f4c085f20c2c"
-#web3 = Web3(Web3.HTTPProvider(rinkeby_link))
-ganache_link = "http://127.0.0.1:7545"
-web3 = Web3(Web3.HTTPProvider(ganache_link))
 
-account_1 = '0x8F95bdb2F2492C8BDc929dDb4C60d15237e193E6' # Fill me in
-account_2 = '0xe3e66d9ba21d358c3dAd781E31cffcD5eB8A53d4' # Fill me in
-private_key = '5a527dcdaa19028c74ded3884c7e9b65ae0b0a577d3e4a4c439e0d9008bce9c5' # Fill me in
+#random_address = Web3.toChecksumAddress("0x6dC0c0be4c8B2dFE750156dc7d59FaABFb5B923D")
 
-nonce = web3.eth.getTransactionCount(account_1)
-receiver = input()
-amount = int(input())
+address = Web3.toChecksumAddress("0x79387b8a5d09e7b1613532a180bdc3d9ef2aab25")
+private_key_hex = "0x431547bea28806bd27a871333e186a05eea3c53ec6a01865f655fd244b2ea497"
+
+rb_link = "https://rinkeby.infura.io/v3/a9ea539cf3a348fead71f4c085f20c2c"
+web3 = Web3(Web3.HTTPProvider(rb_link))
+
+def extract_key(web3):
+    with open('/home/alexey/.ethereum/rinkeby/keystore/UTC--2020-11-08T16-43-51.866107210Z--79387b8a5d09e7b1613532a180bdc3d9ef2aab25') as keyfile:
+        encrypted_key = keyfile.read()
+        return web3.eth.account.decrypt(encrypted_key, '79651603027erased')
+
+def welcome():
+
+    #print('Type your password: ')
+    #unlock_pw = input()
+    #if unlock_pw != 'eth-password': exit()
+
+    nonce = web3.eth.getTransactionCount(address)
+    print('Type in receiving address: ')
+    receiver = input()
+    print('Now, type how much u wanna send')
+    amount = int(input())
+    return (receiver, amount, nonce)
+
+#private_key = extract_key(web3)
+(receiver, amount, nonce) = welcome()
 
 tx = {
     'nonce': nonce,
     'to': receiver,
-    'value': web3.toWei(amount, 'ether'),
+    #'value': web3.toWei(amount, 'ether'),
+    'value': amount,
     'gas': 2000000,
     'gasPrice': web3.toWei('50', 'gwei'),
+    'data': b'',
 }
-signed = web3.eth.account.signTransaction(tx, private_key)
-tx_hash = web3.eth.sendRawTransaction(signed.rawTransaction)
+signed = web3.eth.account.signTransaction(tx, private_key_hex)
+output = web3.eth.sendRawTransaction(signed.rawTransaction)
 #print(web3.toHex(tx_hash))
-print('your transaction is: ', tx)
+print(output)
